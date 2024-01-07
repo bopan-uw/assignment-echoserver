025fdae6f6c702fa8e4c848816dcf3f3cf57536e
bopan-uw <91004152+bopan-uw@users.noreply.github.com>
2024-01-07 00:37:22 -0800

    Change teacher-upstream to assignment-echoserver.git (#1)

--- .github/workflows/check_no_change.sh
+++ .github/workflows/check_no_change.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git remote add teacher-upstream https://github.com/bopan-uw/css432-tftp.git
+git remote add teacher-upstream https://github.com/bopan-uw/assignment-echoserver.git
 git fetch teacher-upstream main:teacher-main
 
 CHANGES=$(git diff teacher-main -- .github/ main -- .github/)
